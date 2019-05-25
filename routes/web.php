79abc44ba3fb6e495c3743ae3dd9b43cedf5e9ce
vagrant <vagrant@homestead>
2019-05-25 16:10:14 +0000

    use artisan make auth

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('guest');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
