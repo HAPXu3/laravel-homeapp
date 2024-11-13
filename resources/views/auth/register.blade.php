1194530181273d1c1d72927b7bfbbfa05b516381
HAPXu3 <narhis_8@mail.ru>
2024-11-13 14:12:14 +0000

    feat: add auth scaffolding

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="row justify-content-center mt-5">
+        <form class="col-3" method="POST">
+            <div class="mb-3">
+                <label for="nickname" class="form-label">Никнейм</label>
+                <input class="form-control" id="nickname" type="text" name="nickname">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Пароль</label>
+                <input class="form-control" id="password" type="password" name="password" aria-describedby="passwordHelp">
+                <div id="passwordHelp" class="form-text">
+                    Пароль должен состоять минимум из 255 символов, включать буквы, цифры, специальные символы, иероглифы, старославянский алфавит
+                </div>
+            </div>
+
+            <button class="btn btn-primary w-100" type="submit">Зарегистрироваться</button>
+        </form>
+    </div>
+@endsection
