3e85aff1594b9071db02e8060bc5dc0f50f16327
HAPXu3 <narhis_8@mail.ru>
2024-11-19 19:06:35 +0000

    feat: add content to main page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="min-vh-100">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,26 +7,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="min-vh-100">
 <div class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
+    <div class="container-fluid container-xl w-75">
         <a class="navbar-brand" href="/">HomeApp</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbar">
+        <div class="collapse navbar-collapse ms-5" id="navbar">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" href="/links">staLINKa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/chat">Chatцкий</a>
                 </li>
             </ul>
         </div>
     </div>
 </div>
 
-<div class="container-fluid">
-    @yield('content')
-</div>
+@yield('content')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.main')
 
 @section('content')
-
+    <div class="container d-flex align-items-center justify-content-center min-vh-100">
+        <div class="row">
+            <div class="col-8 fs-1">Здесь могла быть ваша реклама.</div>
+            <div class="col-8 fs-6">А будет моя.</div>
+        </div>
+    </div>
 @endsection
